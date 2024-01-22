7affa688109afeeb684d75250f446d7c4eafede1
Waylon Dixon <wayjay4@yahoo.com>
2024-01-22 13:48:43 -0700

    implemented intial styling

--- resources/js/Pages/Dashboard.jsx
+++ resources/js/Pages/Dashboard.jsx
@@ -1,5 +1,6 @@
 import AuthenticatedLayout from '@/Layouts/AuthenticatedLayout';
 import { Head } from '@inertiajs/react';
+import GitHubIssues from "@/Pages/Github/GithubIssues.jsx";
 
 export default function Dashboard({ auth }) {
     return (
@@ -9,13 +10,7 @@ export default function Dashboard({ auth }) {
         >
             <Head title="Dashboard" />
 
-            <div className="py-12">
-                <div className="max-w-7xl mx-auto sm:px-6 lg:px-8">
-                    <div className="bg-white dark:bg-gray-800 overflow-hidden shadow-sm sm:rounded-lg">
-                        <div className="p-6 text-gray-900 dark:text-gray-100">You're logged in!</div>
-                    </div>
-                </div>
-            </div>
+            <GitHubIssues />
         </AuthenticatedLayout>
     );
 }
